091da7fc398aeafe2877e98e2fcc5f44f419e4db
Christophe Berbizier (dbdl) <cberbizier@peersuasive.com>
2014-06-30 03:37:57 +0200

    fixed (c)

--- lua_tox.c
+++ lua_tox.c
@@ -4,9 +4,9 @@
  *
  *  Copyright (C) 2014 Peersuasive Technologies All Rights Reserved.
  *
- *  This file is part of Luce.
+ *  This file is part of LuaTox.
  *
- *  Luce is free software: you can redistribute it and/or modify
+ *  LuaTox is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
